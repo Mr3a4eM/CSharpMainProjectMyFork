5bb2a6ae8cf11435c24a893528e8ea573c393897
Alexander <fantqfan@gmail.com>
2025-07-10 12:45:04 +0300

    \EnterPoint.cs
    _timeScale = 5

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -5,13 +5,12 @@ using Model;
 using Model.Config;
 using UnityEngine;
 using Utilities;
-//hello, what the test?
-//Neponyal
+
 public class EnterPoint : MonoBehaviour
 {
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
-    private float _timeScale = 1;
+    private float _timeScale = 5;
     
     void Start()
     {
